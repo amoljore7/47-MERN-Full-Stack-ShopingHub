7f86a520e61c0e8af4bb43b2f8c23b48c71d3830
amol <amol.jore@gslab.com>
2021-02-23 11:48:38 +0530

    Product details Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer } from "./reducers/productReducers";
+import { productDetailsReducer } from "./reducers/productReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
