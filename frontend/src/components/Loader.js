f0e0a0fa3f8fe8e736a0547e75a2d187c4a9f387
amol <amol.jore@gslab.com>
2021-02-23 18:03:59 +0530

    Add To cart Functionality

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -13,7 +13,7 @@ export const Loader = () => {
         display: "block",
       }}
     >
-      <span class="sr-only"> Loading...</span>
+      <span className="sr-only"> Loading...</span>
     </Spinner>
   );
 };
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Cart,
+} from "react-bootstrap";
+import { Message } from "../components/Message";
+import { addToCart } from "../actions/cartAction";
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  console.log(">>>", cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
   return (
     <div>
       <h1>Cart Screen</h1>
