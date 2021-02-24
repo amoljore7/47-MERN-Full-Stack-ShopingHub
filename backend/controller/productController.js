6faf950a9c35f60fbb298d745b96b9e244385b65
amol <amol.jore@gslab.com>
2021-02-24 12:58:16 +0530

    Clean up by using controllers

---
+++ backend/controller/productController.js
@@ -0,0 +1,28 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+// @desc    Fetch all product
+// @route   GET /api/products
+// @access  Publuc
+
+const getProducts = asyncHandler(async (req, res) => {
+  const product = await Product.find({});
+  res.json(product);
+});
+
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Publuc
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product Not Found");
+  }
+});
+
+export { getProductById, getProducts };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,12 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
 
-// @desc    Fetch all product
-// @route   GET /api/products
-// @access  Publuc
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const product = await Product.find({});
-    res.json(product);
-  })
-);
+import {
+  getProducts,
+  getProductById,
+} from "../controller/productController.js";
 
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Publuc
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
