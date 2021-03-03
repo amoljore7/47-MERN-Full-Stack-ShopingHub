6f8a6761466095eaec53a301229166e5a2884179
amol <amol.jore@gslab.com>
2021-03-03 15:04:28 +0530

    user authentication endpoint

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -12,7 +12,7 @@ const getProducts = asyncHandler(async (req, res) => {
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
-// @access  Publuc
+// @access  Public
 
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
