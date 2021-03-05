1a9168f4bb3f76fd21d3991bbc2162e5cace905f
amol <amol.jore@gslab.com>
2021-03-05 14:05:29 +0530

    Show user in Navbar & logout

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -11,19 +11,16 @@ export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return {
-        ...state,
         loading: true,
         products: [],
       };
     case PRODUCT_LIST_SUCCESS:
       return {
-        ...state,
         loading: false,
         products: action.payload,
       };
     case PRODUCT_LIST_FAIL:
       return {
-        ...state,
         loading: false,
         error: action.payload,
       };
@@ -39,18 +36,15 @@ export const productDetailsReducer = (
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
       return {
-        ...state,
         loading: true,
       };
     case PRODUCT_DETAILS_SUCCESS:
       return {
-        ...state,
         loading: false,
         product: action.payload,
       };
     case PRODUCT_DETAILS_FAIL:
       return {
-        ...state,
         loading: false,
         error: action.payload,
       };
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,25 +9,20 @@ export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
       return {
-        ...state,
         loading: true,
       };
     case USER_LOGIN_SUCCESS:
       return {
-        ...state,
         loading: false,
         userInfo: action.payload,
       };
     case USER_LOGIN_FAIL:
       return {
-        ...state,
         loading: false,
         error: action.payload,
       };
     case USER_LOGOUT:
-      return {
-        ...state,
-      };
+      return {};
     default:
       return state;
   }
